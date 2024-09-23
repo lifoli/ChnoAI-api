c6677e06b2d3c4cff834b0e59bffbd266a43c7dc
smreosms13 <smreosms13@gmail.com>
2024-09-23 14:54:42 +0900

    fix: remove api function, calls

--- ai-server/app/test_langgraph.py
+++ ai-server/app/test_langgraph.py
@@ -1,26 +1,20 @@
 from langgraph.graph import StateGraph
 from langgraph.checkpoint.memory import MemorySaver
 from typing import TypedDict
-from test_messages import fetch_messages
-
-# Example URL
-URL_EXAMPLE_1 = "https://chatgpt.com/share/68e6ee7c-23ee-4527-bf3d-7d4d4f4e98f8"
-URL_EXAMPLE_2 = "https://chatgpt.com/share/6de493cf-d959-4b58-8cab-338f665ee851"
+from test_messages import fetch_messages, CONVERSATION_ID_EXAMPLE_1, CONVERSATION_ID_EXAMPLE_2
 
 class GraphState(TypedDict):
-    conversation_id: str
+    conversation_id: int
     messages: list
-    url: str
 
 def get_processed_graph_state(state:GraphState) -> GraphState:
-    url= state["url"]
+    conversation_id= state["conversation_id"]
     
-    final_conversation_id, final_messages = fetch_messages(url)
+    messages = fetch_messages(conversation_id)
 
     processed_graph_state = GraphState(
-      conversation_id=final_conversation_id,
-      messages=final_messages,
-      url=graph_state["url"]  
+      conversation_id=conversation_id,
+      messages=messages,
     )
 
     return processed_graph_state  
@@ -38,9 +32,8 @@ compiled_graph = fetch_messages_graph.compile(checkpointer=memory)
 
 # 들어갈 graph_state를 정의
 graph_state = GraphState(
-    conversation_id="", 
+    conversation_id=CONVERSATION_ID_EXAMPLE_1, # 테스트 할 conversation id로 변경
     messages=[],
-    url=URL_EXAMPLE_1  # 호출할 URL -> 테스트할 URL 변경
 )
 
 final_state = compiled_graph.invoke(
--- ai-server/app/test_messages.py
+++ ai-server/app/test_messages.py
@@ -1,42 +1,37 @@
-import requests
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
 
-# Example URL
-URL_EXAMPLE_1 = "https://chatgpt.com/share/68e6ee7c-23ee-4527-bf3d-7d4d4f4e98f8"
-URL_EXAMPLE_2 = "https://chatgpt.com/share/6de493cf-d959-4b58-8cab-338f665ee851"
+load_dotenv()
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
-# URL로 메세지 데이터 조회
-def fetch_messages(url, user_id=11) -> dict:
-    
-    # url -> tech_note, conversation
-    tech_note_response = requests.post(
-        'http://localhost:4000/create/link', 
-        json={"url": url, "userId": user_id}
-    )
+database: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+CONVERSATION_ID_EXAMPLE_1 = 146
+CONVERSATION_ID_EXAMPLE_2 = 152
+
+# conversation_id로  메세지 데이터 조회
+def fetch_messages(conversation_id) -> list:
+    try:
+        response = database.table("messages") \
+                    .select("sequence_number, message_type, message_content") \
+                    .eq("conversation_id", conversation_id) \
+                    .order("sequence_number", desc=False) \
+                    .execute()
         
-    if tech_note_response.status_code != 201:
-        print(f"Failed to create tech note: {tech_note_response.status_code}, {tech_note_response.text}")
-        return "", []
+        messages = response.data
+        if len(messages) == 0 :
+            raise Exception(f"No messages related conversation {conversation_id}")
 
-    tech_note_data = tech_note_response.json().get("techNoteData")
-    conversation_id = tech_note_data.get("conversation_id")
+    except Exception as e:
+        print(f"Internal server error: {str(e)}") 
 
-    if conversation_id == "" :
-      print("Not Found Convesation ID")
-      return "", []
+    
+    return messages
 
-    # conversation ID -> messages           
-    message_response = requests.get(f'http://localhost:4000/messages/{conversation_id}')  
+# Usage example, 테스트할 conversation_id로 변경
+messages = fetch_messages(CONVERSATION_ID_EXAMPLE_1) 
 
-    if message_response.status_code == 200:
-        data = message_response.json()
-        messages = data.get("messages", [])     
-    else:
-        print(f"Failed to fetch messages: {message_response.status_code}, {message_response.text}")
-        messages = []
-
-    return conversation_id, messages
-
-
-# Usage example, 테스트할 URL로 변경
-convesation_id, messages = fetch_messages(URL_EXAMPLE_1) 
+# print(messages)
 
