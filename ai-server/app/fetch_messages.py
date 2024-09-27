7bba3d94eaf781edafcdcb42db13f327aeaf9a9c
smreosms13 <smreosms13@gmail.com>
2024-09-28 03:59:31 +0900

    fix: fetch_message return type 변경 및 파일명  변경

--- ai-server/app/fetch_messages.py
+++ ai-server/app/fetch_messages.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
+from typing import TypedDict
 
 load_dotenv()
 SUPABASE_URL = os.getenv('SUPABASE_URL')
@@ -11,11 +12,18 @@ database: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 CONVERSATION_ID_EXAMPLE_1 = 146
 CONVERSATION_ID_EXAMPLE_2 = 152
 
+class Message(TypedDict):
+    id: int
+    conversation_id: int
+    sequence_number: int
+    message_type: str
+    message_content: str
+
 # conversation_id로  메세지 데이터 조회
-def fetch_messages(conversation_id) -> list:
+def fetch_messages(conversation_id: int) -> list[Message]:
     try:
         response = database.table("messages") \
-                    .select("sequence_number, message_type, message_content") \
+                    .select("id, conversation_id, sequence_number, message_type, message_content") \
                     .eq("conversation_id", conversation_id) \
                     .order("sequence_number", desc=False) \
                     .execute()
