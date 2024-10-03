54c9b4d4718a4fdd9c8110e5e3a339b15495da4d
kyujini34_gram <kyujini34@naver.com>
2024-10-03 18:19:02 +0900

    writer 모듈 그래프 형식으로 바꾸기

--- ai-server/app/writer.py
+++ ai-server/app/writer.py
@@ -6,7 +6,6 @@ from tabnanny import check
 from certifi import contents
 from dotenv import load_dotenv
 load_dotenv()
-import json
 
 # langchin
 from langchain_upstage import ChatUpstage
@@ -22,46 +21,31 @@ from langfuse import Langfuse
 from langfuse.callback import CallbackHandler
 
 from evaluation_utils import EvaluationUtils
-#from question_compressor import process_single_q_and_a
-
-#################################################################
-# example1
-EXAMPLE1_CONVERSATION_ID = 146
-
-# example5
-EXAMPLE5_CONVERSATION_ID = 162
-
-# example6
-EXAMPLE6_CONVERSATION_ID = 153
-#################################################################
-
-evaluation_utils = EvaluationUtils()
-langfuse_handler = CallbackHandler()
-langfuse = Langfuse()
-model = ChatUpstage(model="solar-pro")
 
 class q_and_a(TypedDict):
     q: str
     a: str
 
-class GraphState(TypedDict):
-    not_processed_conversations: list[q_and_a]
-    processed_conversations: list[q_and_a]
-    result: str  
+class GraphState(TypedDict): 
     preprocessed_conversations: list[q_and_a]
     code_document: dict
     message_to_index_dict: dict
     final_documents: dict
-
     '''
     Writing 모듈 실행을 위해 필요한 Inputs
-    1. preprocessed QA sets: list[q_and_a] 
-    2. code_document: list
-    3. 각각의 QA에 해당하는 indices: dict[message_id: index_list]
-    4. 작성 중인 문서들: dict[index: content]
+    1. preprocessed_conversations -> 서현님 모듈에서 전처리된 QA 세트: list[q_and_a] 
+    2. code_document -> 서현님 모듈에서 만든 코드 딕셔너리 dict{'Code_Snippet_1': 'code'}
+    3. message_to_index_dict -> 지환님 모듈에서 만든 각 QA 세트에 해당하는 indices: dict['0': [1-1, 1-2, 1-3]]
+    4. final_documents -> 작성 중인 문서들: dict['1-1': '1-1) heading \n content']
     '''
 
-# 이하는 evaluation functions
+evaluation_utils = EvaluationUtils()
+langfuse_handler = CallbackHandler()
+langfuse = Langfuse()
+
+import re
+
+##########################평가 관련 함수 정의###############################
 
 # code_snippet 찾는 함수
 def find_code_snippets(text):
@@ -106,7 +90,9 @@ def overall_precision_recall(generated_doc, gt_doc):
     
     return precision, recall
 
-# 이하는 writer 모듈 동작을 위한 functions
+##########################평가 관련 함수 정의###############################
+
+##########################블로그 초안 작성하는 노드와 관련 함수 정의###############################
 
 def write(model, q_and_a, document):
     writing_prompt = langfuse.get_prompt("writing_prompt")
@@ -123,30 +109,28 @@ def remove_after_second_hashes(text):
         return '## ' + parts[1].strip()  # '##' 포함 첫 번째 부분만 반환
     return text  # '##'가 두 번 이상 없을 때는 원래 문자열 반환
 
-def make_final_documents(graph_state: GraphState, model):
-    preprocessed_conversations = graph_state['preprocessed_conversations']
+def make_final_documents(state: GraphState):
+    preprocessed_conversations = state['preprocessed_conversations']
     for i in range(len(preprocessed_conversations)):
         qa = preprocessed_conversations[i]
-        indices_for_qa = graph_state['message_to_index_dict'][str(i)]
-        print('QA', i, 'processing...')
+        indices_for_qa = state['message_to_index_dict'][str(i)]
+        #print('QA', i, 'processing...')
         for index in indices_for_qa:
-            document = graph_state['final_documents'][index]
+            document = state['final_documents'][index]
             flag = True
             while(flag):
                 generated_doc, _ = write(model, qa, document)
                 updated_doc = remove_after_second_hashes(generated_doc.content)
                 if not ('[Q]' in updated_doc):
                     flag = False
-            graph_state['final_documents'][index] = updated_doc
-            print('doc', index, '...')
-    return 0
+            state['final_documents'][index] = updated_doc
+            #print('doc', index, '...')
+    return state
 
-# 최종 doc에서 중복되는 코드 삭제하는 부분 (refinement)
-'''
-입력: 
-code_document
-generated_document
-'''
+##########################블로그 초안 작성하는 노드와 관련 함수 정의###############################
+
+##################블로그 초안을 받아 하나의 코드가 하나의 목차에만 들어가게 수정하는 노드와 관련 함수 정의##############
+
 def extract_heading(text):
     start_index = text.find("##")
     if start_index == -1: return ""
@@ -179,12 +163,12 @@ def make_heading_list_for_prompt(heading_list):
         text = text + heading + '\n'
     return text[:-1]
 
-def document_refinement(state: GraphState, model):
-    code_list = list(loaded_data['EXAMPLE9']['code_document'].keys()) # 이 부분 차후에 수정해야 한다. 현재는 GT 데이터로부터 가져옴
+def document_refinement(state: GraphState):
+    code_list = list(loaded_data['EXAMPLE9']['code_document'].keys())
     document_refinement_1 = langfuse.get_prompt("document_refinement_1")
     document_refinement_2 = langfuse.get_prompt("document_refinement_2")
     for code_id in code_list:
-        print(code_id, 'processing...')
+        #print(code_id, 'processing...')
         indices_list, heading_list, whole_snippet = find_indices_and_snippet_with_code_id(code_id, state['final_documents'])
         indices = make_heading_list_for_prompt(heading_list)
 
@@ -192,13 +176,17 @@ def document_refinement(state: GraphState, model):
         selected = model.invoke(prompt)
         selected = selected.content
         for index in indices_list:
-            print(index, '...')
+            #print(index, '...')
             if selected != index:
                 doc = state['final_documents'][index]
                 prompt = document_refinement_2.compile(code_snippet=whole_snippet, doc=doc)
                 updated = model.invoke(prompt)
                 state['final_documents'][index] = updated.content
-    return 0
+    return state
+
+##################블로그 초안을 받아 하나의 코드가 하나의 목차에만 들어가게 수정하는 노드와 관련 함수 정의##############
+
+######################작성한 블로그의 코드 스니펫을 원래 코드로 교체 및 헤딩 표시(#) 지우기######################
 
 def replace_code_snippets(document, snippets_dict):
     # Code_Snippet_1 및 Code_Snippet_2 키를 사용해 대체할 패턴을 찾음
@@ -218,34 +206,67 @@ def make_blog(state: GraphState):
         t = t.lstrip(' ')
         state['final_documents'][index] = t
         #print(t, end='\n\n')
+    return state
 
-# 아래는 example9 데이터가 들어있는 json 파일에서 데이터를 불러와 테스트해보는 코드
+######################작성한 블로그의 코드 스니펫을 원래 코드로 교체 및 헤딩 표시(#) 지우기######################
+
+##########################그래프 내의 요소(node, edge)들을 정의###############################3
+# 메모리를 정의
+memory = MemorySaver() 
+
+# 새로운 graph 정의
+writer_graph = StateGraph(GraphState)
+
+# 사용할 node를 정의(다른 단계를 수행할 node를 제작하고 싶다면 여기에 node를 추가)
+writer_graph.add_node("블로그 초안 작성", make_final_documents)
+writer_graph.add_node("블로그 글 다듬기", document_refinement)
+writer_graph.add_node("블로그 글 후처리", make_blog)
+
+
+# 그래프 시작점정의
+writer_graph.set_entry_point("블로그 초안 작성")
+
+# Define Edges
+writer_graph.add_edge('블로그 초안 작성', "블로그 글 다듬기")
+writer_graph.add_edge("블로그 글 다듬기", "블로그 글 후처리")
+##########################그래프 내의 요소(node, edge)들을 정의###############################3
+
+################ 아래는 example9 데이터가 들어있는 json 파일에서 데이터를 불러와 테스트해보는 코드 ################
+
+import json
 
 with open('data_for_writing.json', 'r', encoding='utf-8') as json_file:
     loaded_data = json.load(json_file)
 
+model = ChatUpstage(model="solar-pro")
+    
+##########################그래프 실행###############################
+# 그래프를 컴파일
+compiled_graph = writer_graph.compile(checkpointer=memory)
+
+# 들어갈 graph_state를 정의
 graph_state = GraphState(
-    not_processed_conversations=loaded_data['EXAMPLE9']['conversations'],
-    processed_conversations=None,
-    result=None,
     preprocessed_conversations=loaded_data['EXAMPLE9']['preprocessed_conversations'],
     code_document=loaded_data['EXAMPLE9']['code_document'],
     message_to_index_dict=loaded_data['EXAMPLE9']['message_to_index_dict'],
-    final_documents=loaded_data['EXAMPLE9']['final_documents']
+    final_documents=loaded_data['EXAMPLE9']['final_documents'],
+    eval=None
 )
 
-# write
-make_final_documents(graph_state, model)
-# refinement
-document_refinement(graph_state, model)
-# 앞의 # 제거하고 코드도 실제 코드로 교체
-make_blog(graph_state)
+# 그래프를 실행
+final_state = compiled_graph.invoke(
+    graph_state, 
+    config={
+        "configurable": {"thread_id": 42}, 
+        "callbacks": [langfuse_handler]}
+)
+##########################그래프 실행###############################
 
-# eval
-precision, recall = overall_precision_recall(graph_state['final_documents'], loaded_data['EXAMPLE9']['GT_document'])
-print(f"Overall Precision: {precision:.2f}")
-print(f"Overall Recall: {recall:.2f}")
+# # eval
+# precision, recall = overall_precision_recall(graph_state['final_documents'], loaded_data['EXAMPLE9']['GT_document'])
+# print(f"Overall Precision: {precision:.2f}")
+# print(f"Overall Recall: {recall:.2f}")
 
-# 전체 블로그 출력
-for text in list(graph_state['final_documents'].values()):
+# 만든 최종 그래프 출력
+for text in list(final_state['final_documents'].values()):
     print(text, end='\n\n')
